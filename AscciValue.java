6e31c01e6dfe0f6e10386961b654f84b3f4a8a37
Nishathsultana <86568156+Nishathsultana@users.noreply.github.com>
2021-07-11 03:05:05 +0530

    Add files via upload

---
+++ AscciValue.java
@@ -0,0 +1,15 @@
+
+public class AscciValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='H';
+		int ascii=ch;
+		// type casting can also be done 
+		int castAscii=(int) ch;
+		 System.out.println("The Ascii value of "  +ch+ "  is "+ascii);
+		 System.out.println("The Ascii value of "  +ch+ " is"+castAscii);
+
+	}
+
+}
